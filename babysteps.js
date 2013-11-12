37b2001cbb0bc4130c1bd5bd6749c4fe645d1680
Paul O'Connell <paul.t.oconnell@gmail.com>
2013-11-12 07:34:21 +0000

    util.format

--- babysteps.js
+++ babysteps.js
@@ -1,4 +1,14 @@
+require('util');
+
+console.log(process.argv[0]);
+console.log(process.argv[1]);
+console.log(Number(process.argv[2]));
+console.log(Number(process.argv[3]));
+console.log(Number(process.argv[4]));
+console.log(util.format('%d:%d',process.argv[2],process.argv[3]));
+
 process.argv.forEach(function(val, index, array) {
-  console.log(index+':'+val + ',');
-  //console.log(array);
+  console.log(index+':'+val+',');
+  if(index>=2)
+	console.log(Number(val));
 });
