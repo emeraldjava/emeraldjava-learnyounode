4384ae58222275bfaf8bed2f9964312ec4a950f4
Paul O'Connell <paul.t.oconnell@gmail.com>
2014-07-12 14:02:24 +0100

    first io

---
+++ firstio.js
@@ -0,0 +1,7 @@
+var util = require('util');
+var fs = require('fs');
+
+var file = fs.readFileSync(process.argv[2]);
+var str = file.toString('utf-8', 0, file.length);
+var lines = str.split("\n");
+console.log(lines.length-1);
