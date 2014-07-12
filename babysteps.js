eeb1c20de856ba5f8bbd5de3c69e5022faba0e02
Paul O'Connell <paul.t.oconnell@gmail.com>
2014-07-12 13:33:08 +0100

    babysteps code tidy

--- babysteps.js
+++ babysteps.js
@@ -1,17 +1,8 @@
 var util = require('util');
 
-//console.log(process.argv[0]);
-//console.log(process.argv[1]);
-//console.log(Number(process.argv[2]));
-//console.log(Number(process.argv[3]));
-//console.log(Number(process.argv[4]));
-//console.log(util.format('%d:%d',process.argv[2],process.argv[3]));
-
 var sum = 0;
 process.argv.forEach(function(val, index, array) {
-  //console.log(index+':'+val+',');
   if(index>=2)
 	sum += Number(val);
-	//console.log(Number(val));
 });
 console.log(sum);
