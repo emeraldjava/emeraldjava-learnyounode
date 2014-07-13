8bce9e2e45b9acebf4f9f3cb86cda94359ae142f
Paul O'Connell <paul.t.oconnell@gmail.com>
2014-07-13 12:35:28 +0100

    http client

---
+++ httpclient.js
@@ -0,0 +1,14 @@
+var http = require('http');
+
+var request = http.get(process.argv[2], function(response) {
+  //console.log("Got response: " + response.statusCode);
+  //console.log('STATUS: ' + response.statusCode);
+  //console.log('HEADERS: ' + JSON.stringify(response.headers));
+  response.setEncoding('utf8');
+  response.on('data', function (chunk) {
+    console.log(chunk);
+  });
+  response.on('error',console.error);
+}).on('error', function(e) {
+  console.log("Got error: " + e.message);
+})
